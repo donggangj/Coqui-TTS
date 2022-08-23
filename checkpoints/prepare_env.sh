7ee561e26fbee895815d7a69ac9577351c841a21
Jiang, Donggang <donggang.jiang@intel.com>
2022-08-23 09:05:33 +0800

    Fix prepare_env.sh

--- checkpoints/prepare_env.sh
+++ checkpoints/prepare_env.sh
@@ -2,9 +2,9 @@ python -m pip install -e ../
 python -m pip install pydub ffmpeg-normalize==1.21.0
 python -m pip install torchaudio==0.9.0
 
-CHECKPOINT_SE_PATH = "SE_checkpoint.pth.tar"
-CHECKPOINT_MODEL_PATH = "best_model.pth.tar"
-SPKR_EMB_PATH = "speakers.json"
+CHECKPOINT_SE_PATH="SE_checkpoint.pth.tar"
+CHECKPOINT_MODEL_PATH="best_model.pth.tar"
+SPKR_EMB_PATH="speakers.json"
 gdown --id 17JsW6h6TIh7-LkU2EvB_gnNrPcdBxt7X -O $CHECKPOINT_SE_PATH
 gdown --id 1sgEjHt0lbPSEw9-FSbC_mBoOPwNi87YR -O $CHECKPOINT_MODEL_PATH
 gdown --id 1SZ9GE0CBM-xGstiXH2-O2QWdmSXsBKdC -O $SPKR_EMB_PATH
